0f46c997cd5e63ad3f20e503839f218e93c26808
Adite_React <vipuljikadra@Adite-Reacts-Mac-mini.local>
2020-09-09 18:35:07 +0530

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "ap-south-1",
+    BUCKET: "adite-notes-app-uploads",
+  },
+  apiGateway: {
+    REGION: "ap-south-1",
+    URL: "https://8fpieenw89.execute-api.ap-south-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "ap-south-1",
+    USER_POOL_ID: "ap-south-1_lNnLORyB3Pool",
+    APP_CLIENT_ID: "4afkind566ip0qa3clqqas51dt",
+    IDENTITY_POOL_ID: "ap-south-1:6a54b8bd-0a33-4945-83ab-35994abf7b6d",
+  },
+};
